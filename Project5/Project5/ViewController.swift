15f4e34b65e84020fd89995108f1d0fc82e1b3d2
Derya Cayiroglu <deryacayroglu@gmail.com>
2023-07-04 10:28:38 +0300

    Project created

---
+++ Project5/Project5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project5
+//
+//  Created by Derya Çayıroğlu on 4.07.2023.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

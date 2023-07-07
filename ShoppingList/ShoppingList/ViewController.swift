2143716d3d9d49ce55e5674bcb57d949d65cb7cd
Derya Cayiroglu <deryacayroglu@gmail.com>
2023-07-07 10:12:54 +0300

    project created

---
+++ ShoppingList/ShoppingList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShoppingList
+//
+//  Created by Derya Çayıroğlu on 7.07.2023.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

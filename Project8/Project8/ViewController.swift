9c605c22f37e7d9204fb77dc746f9c731eb494ea
Derya Cayiroglu <deryacayroglu@gmail.com>
2023-07-12 20:00:27 +0300

    letterButtons added

--- Project8/Project8/ViewController.swift
+++ Project8/Project8/ViewController.swift
@@ -29,6 +29,7 @@ class ViewController: UIViewController {
         clueslabel.font = UIFont.systemFont(ofSize: 24)
         clueslabel.text = "CLUES"
         clueslabel.numberOfLines = 0
+        clueslabel.setContentHuggingPriority(UILayoutPriority(1), for: .vertical)
         view.addSubview(clueslabel)
         
         answersLabel = UILabel()
@@ -37,6 +38,7 @@ class ViewController: UIViewController {
         answersLabel.text = "ANSWER"
         answersLabel.textAlignment = .right
         answersLabel.numberOfLines = 0
+        answersLabel.setContentHuggingPriority(UILayoutPriority(1), for: .vertical)
         view.addSubview(answersLabel)
         
         currentAnswer = UITextField()
@@ -56,6 +58,10 @@ class ViewController: UIViewController {
         clear.translatesAutoresizingMaskIntoConstraints = false
         clear.setTitle("CLEAR", for: .normal)
         view.addSubview(clear)
+        
+        let buttonsView = UIView()
+        buttonsView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(buttonsView)
         
         NSLayoutConstraint.activate([
             scoreLabel.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor),
@@ -81,12 +87,34 @@ class ViewController: UIViewController {
             
             clear.centerXAnchor.constraint(equalTo: view.centerXAnchor,constant: 100),
             clear.centerYAnchor.constraint(equalTo: submit.centerYAnchor),
-            clear.heightAnchor.constraint(equalToConstant: 44)
+            clear.heightAnchor.constraint(equalToConstant: 44),
+            
+            buttonsView.widthAnchor.constraint(equalToConstant: 750),
+            buttonsView.heightAnchor.constraint(equalToConstant: 320),
+            buttonsView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            buttonsView.topAnchor.constraint(equalTo: submit.bottomAnchor, constant: 20),
+            buttonsView.bottomAnchor.constraint(equalTo: view.layoutMarginsGuide.bottomAnchor, constant: -20)
+            
             
         ])
         
-        clueslabel.backgroundColor = .red
-        answersLabel.backgroundColor = .blue
+        let width = 150
+        let height = 80
+        
+        for row in 0..<4 {
+            for column in 0..<5 {
+                let letterButton = UIButton(type: .system)
+                letterButton.titleLabel?.font = UIFont.systemFont(ofSize: 36)
+                letterButton.setTitle("WWW", for: .normal)
+                
+                let frame = CGRect(x: column * width, y: row * height, width: width, height: height)
+                letterButton.frame = frame
+                
+                buttonsView.addSubview(letterButton)
+                letterButtons.append(letterButton)
+            }
+        }
+        
     }
     
 

c12b217d5d7ea73d44fe5460caa3e94fdb5df429
Derya Cayiroglu <deryacayroglu@gmail.com>
2023-07-12 19:00:28 +0300

    labels added

--- Project8/Project8/ViewController.swift
+++ Project8/Project8/ViewController.swift
@@ -8,6 +8,56 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var clueslabel: UILabel!
+    var answersLabel: UILabel!
+    var currentAnswer: UITextField!
+    var scoreLabel: UILabel!
+    var letterButtons = [UIButton]()
+    
+    override func loadView() {
+        view = UIView()
+        view.backgroundColor = .white
+        
+        scoreLabel = UILabel()
+        scoreLabel.translatesAutoresizingMaskIntoConstraints = false
+        scoreLabel.textAlignment = .right
+        scoreLabel.text = "Score: 0"
+        view.addSubview(scoreLabel)
+        
+        clueslabel = UILabel()
+        clueslabel.translatesAutoresizingMaskIntoConstraints = false
+        clueslabel.font = UIFont.systemFont(ofSize: 24)
+        clueslabel.text = "CLUES"
+        clueslabel.numberOfLines = 0
+        view.addSubview(clueslabel)
+        
+        answersLabel = UILabel()
+        answersLabel.translatesAutoresizingMaskIntoConstraints = false
+        answersLabel.font = UIFont.systemFont(ofSize: 24)
+        answersLabel.text = "ANSWER"
+        answersLabel.numberOfLines = 0
+        view.addSubview(answersLabel)
+        
+        NSLayoutConstraint.activate([
+            scoreLabel.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor),
+            scoreLabel.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor),
+            
+            clueslabel.topAnchor.constraint(equalTo: scoreLabel.bottomAnchor),
+            clueslabel.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor, constant: 100),
+            clueslabel.widthAnchor.constraint(equalTo: view.layoutMarginsGuide.widthAnchor, multiplier: 0.6, constant: -100),
+            
+            answersLabel.topAnchor.constraint(equalTo: scoreLabel.bottomAnchor),
+            answersLabel.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor, constant: -100),
+            answersLabel.widthAnchor.constraint(equalTo: view.layoutMarginsGuide.widthAnchor, multiplier: 0.4, constant: -100),
+            
+            answersLabel.heightAnchor.constraint(equalTo: clueslabel.heightAnchor)
+            
+        ])
+        
+        clueslabel.backgroundColor = .red
+        answersLabel.backgroundColor = .blue
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()

9e7a3f4c78926db9f2370aef408851910f61e6a5
Derya Cayiroglu <deryacayroglu@gmail.com>
2023-07-18 18:01:23 +0300

    personCell file added

---
+++ Project10/Project10/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by Derya Çayıroğlu on 18.07.2023.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var name: UILabel!
+}

71c7fb51bf69b297c2bf8fef431849bf2e3d38d2
Derya Cayiroglu <deryacayroglu@gmail.com>
2023-07-27 15:32:19 +0300

    interface designed

--- Project13/Project13/ViewController.swift
+++ Project13/Project13/ViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var intensity: UISlider!
+    @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func changeFilter(_ sender: Any) {
+    }
+    
+    @IBAction func save(_ sender: Any) {
+    }
+    
+    
+    @IBAction func intensityChanged(_ sender: Any) {
+    }
 }
 

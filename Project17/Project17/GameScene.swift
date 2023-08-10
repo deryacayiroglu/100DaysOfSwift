faa1bfb9ba4ad53e14627e38bda840615bf75335
Derya Cayiroglu <deryacayroglu@gmail.com>
2023-08-10 16:22:57 +0300

    project created

---
+++ Project17/Project17/GameScene.swift
@@ -0,0 +1,20 @@
+//
+//  GameScene.swift
+//  Project17
+//
+//  Created by Derya Çayıroğlu on 10.08.2023.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    
+    override func didMove(to view: SKView) {
+        
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+}

188166596a551bf3ac91e255dce09110dab8e66a
Derya Cayiroglu <deryacayroglu@gmail.com>
2023-08-19 16:36:16 +0300

    project created

---
+++ Project21/Project21/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project21
+//
+//  Created by Derya Çayıroğlu on 19.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

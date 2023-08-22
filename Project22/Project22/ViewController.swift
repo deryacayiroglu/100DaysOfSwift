2736aa58343f19823bdf085ee90f82fc8bec26c7
Derya Cayiroglu <deryacayroglu@gmail.com>
2023-08-22 16:15:51 +0300

    location privacy added

--- Project22/Project22/ViewController.swift
+++ Project22/Project22/ViewController.swift
@@ -5,13 +5,32 @@
 //  Created by Derya Çayıroğlu on 22.08.2023.
 //
 
+import CoreLocation
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    @IBOutlet weak var distanceReading: UILabel!
+    var locationManager: CLLocationManager?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        locationManager = CLLocationManager()
+        locationManager?.delegate = self
+        locationManager?.requestAlwaysAuthorization()
+        
+        view.backgroundColor = .gray
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedAlways {
+            if CLLocationManager.isMonitoringAvailable(for: CLBeaconRegion.self){
+                if CLLocationManager.isRangingAvailable() {
+                    // do stuff
+                }
+            }
+        }
     }
 
 
